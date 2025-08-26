706decb2cf2de64dc4749e2bc669d94d4fddedb2
Memud <mmemud@coloradomesa.edu>
2025-08-26 14:55:37 -0600

    Initial CMU Housing Prediction app deployment13

--- app.py
+++ app.py
@@ -218,7 +218,7 @@ rf_model, training_features, feature_importance, X, y = load_trained_model()
 # Sidebar Navigation - centered and bigger logo
 col1, col2, col3 = st.sidebar.columns([1, 2, 1])
 with col2:
-    st.image("Picture1.png", width=350)
+    st.image("Picture1.png", width=600)
 
 st.sidebar.markdown("---")
 
