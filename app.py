8e4b2b51ce543b47e8b09c9a081f55d5cd4bb2d7
Muiz Memud <143113219+walememud@users.noreply.github.com>
2025-08-26 15:04:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -205,6 +205,11 @@ def load_trained_model():
         return None, [], pd.DataFrame(), None, None
 
 # Header
+st.markdown("""
+<div style='margin-top: -50px; margin-bottom: -20px;'>
+</div>
+""", unsafe_allow_html=True)
+
 col1, col2, col3 = st.columns([1, 1, 1])
 with col2:
     st.image("Picture2.png", width=400)  # This will actually be larger
